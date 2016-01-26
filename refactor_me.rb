b925adaa0e01ae4f80193dc048516500fdc95b4d
Jessica Ziegler <jziegler@lsoc.org>
2016-01-26 16:37:25 -0600

    fixed

--- refactor_me.rb
+++ refactor_me.rb
@@ -3,18 +3,20 @@ first_name = gets.chomp
 puts "Enter your Last name"
 last_name = gets.chomp
 puts "enter your age"
-age = gets
-if age <= 15
-puts "Enjoy life while you're young kid."
-elsif age = 16
-puts " What is your favorite car"
+age = gets.chomp.to_i
+if age < 15.0
+	puts "Enjoy life while you're young kid."
+elsif age == 15.0
+	puts "Enjoy life while you're young kid."
+elsif age == 16.0
+	puts "What is your favorite car"
 car = gets.chomp
-puts "better get a job, a #{car.downcase} doesn't buy itself"
-elseif age > 16
-puts "What do you want to be when you grow up?"
-dream = "gets.chomp"
-puts "I'll see you in 5 years, " + "first_name.capital" + last_name.capital. + "The #{dream}.".upcase
-elsif > 18
-"Woo! #{first_name.capitalize} is an adult!"
-"Watch out world!"
+	puts "better get a job, a #{car.downcase} doesn't buy itself"
+elsif age < 18.0 or age < 16.0
+	puts "What do you want to be when you grow up?"
+dream = gets.chomp
+	puts "I'll see you in 5 years, " + "#{first_name.upcase}" + " #{last_name.upcase}" + " the #{dream.upcase}"
+elsif age > 18
+	puts "Woo! #{first_name.capitalize} is an adult!"
+	puts "Watch out world!"
 end
